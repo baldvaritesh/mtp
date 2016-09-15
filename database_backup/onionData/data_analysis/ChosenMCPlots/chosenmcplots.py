79b88bfab7f75b8a78242f05ccc4e88508edf37f
baldvaritesh <baldvaritesh@gmail.com>
2016-09-15 20:05:19 +0530

    Plot Creating Library Defined | Use it to create Plots | Find any observations

--- database_backup/onionData/data_analysis/ChosenMCPlots/chosenmcplots.py
+++ database_backup/onionData/data_analysis/ChosenMCPlots/chosenmcplots.py
@@ -6,11 +6,54 @@ from collections import Counter
 # Lib Functions
 ##################################################################################
 
-def CreateSeries(Centre, Mandis, Retail, Whole):
+def CreateCentreSeries(Centre, Retail, EWMAisOn):
   rc = Retail[Retail[1] == Centre]
   rc = rc.sort([0], ascending=[True])
   rc[3] = rc.apply(lambda row: datetime.strptime(row[0], '%Y-%m-%d'), axis=1)
   rc.drop(rc.columns[[0,1]], axis=1, inplace=True)
+  rc.set_index(3, inplace=True)
+  rc.index.names=[None]
+  idx = pd.date_range('2006-01-01', '2015-06-23')
+  rc = rc.reindex(idx, fill_value=0)
+  # Check this out
+  rcSeries = []
+  if (EWMAisOn):
+    rcSeries = pd.ewma(rc[2], span=7)
+  else:
+    rcSeries = rc
+  return rcSeries*100
+
+def CreateMandiSeries(Mandi, Whole, EWMAisOn):
+  mc = Whole[Whole[1] == Mandi]
+  mc = mc.sort([0], ascending=[True])
+  mc[8] = mc.apply(lambda row: datetime.strptime(row[0], '%Y-%m-%d'), axis=1)
+  mc.drop(mc.columns[[0,1,2,3,4,5,6]], axis=1, inplace=True)
+  mc.set_index(8, inplace=True)
+  mc.index.names=[None]
+  idx = pd.date_range('2006-01-01', '2015-06-23')
+  mc = mc.reindex(idx, fill_value=0)
+  mcSeries = []
+  if (EWMAisOn):
+    mcSeries = pd.ewma(mc[7], span=7)
+  else:
+    mcSeries = mc
+  return mcSeries
+
+def PlotDifferenceAbsolute(SeriesA, SeriesB, MCDiff):
+  diff = abs(SeriesA - SeriesB) if (not MCDiff) else abs(SeriesA[2] - SeriesB[7])
+  plt.plot(diff)
+  plt.show()
+
+def PlotDifference(SeriesA, SeriesB, MCDiff):
+  diff = (SeriesA - SeriesB) if (not MCDiff) else (SeriesA[2] - SeriesB[7])
+  plt.plot(diff)
+  plt.show()
+
+def PlotCentreWithMandi(CSeries, MSeries):
+  plt.plot(CSeries)
+  plt.plot(MSeries)
+  plt.show()
+
 
 ##################################################################################
 # Generate Plots
@@ -39,18 +82,19 @@ retail = retail[retail[0] <= "2015-06-23"]
 whole = whole.drop_duplicates(subset=[0,1], take_last=True)
 retail = retail.drop_duplicates(subset=[0,1], take_last=True)
 
-rc = retail[retail[1] == 37]
-rc = rc.sort([0], ascending=[True])
-rc[3] = rc.apply(lambda row: datetime.strptime(row[0], '%Y-%m-%d'), axis=1)
-rc.drop(rc.columns[[0,1]], axis=1, inplace=True)
-rc.set_index(3, inplace=True)
-rc.index.names=[None]
-idx = pd.date_range('2006-01-01', '2015-06-23')
-# Check this out
-# pd.ewma(rc[2], span=7)
-rc = rc.reindex(idx, fill_value=0)
-
 
 # Now for the assigned centres and mandis create series
+x = {37: 0, 40: 1, 50: 2}
 c = [37,40,50]
 m = [[337, 345, 577], [278, 284, 285, 288, 323, 324, 405, 545, 584], [279, 325, 376]]
+
+cs = []
+ms = []
+
+for i in range(0 , len(c)):
+  cs.append(CreateCentreSeries(c[i] , retail, True))
+  ms.append([])
+  for j in range(0, len(m[i])):
+    ms[i].append(CreateMandiSeries(m[i][j] , whole, True))
+
+
